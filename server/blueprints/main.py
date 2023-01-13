74e415039c1a7b7a101f74742f72d50b2ae865da
LukeyBit <lukas@lukasandersson.com>
2023-01-13 13:47:55 +0100

    Switched to react-nodejs

--- server/blueprints/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-m = Blueprint('main', __name__)
-
-@m.route('/')
-def index():
-    return 'This is the index page'
--- server/blueprints/user.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-u = Blueprint('user', __name__)
-
-@u.route('/user')
-def user():
-    return 'This is the user page'
