a59bcf426cdc7741a5f094dbdcdf7c1fc444cd76
LukeyBit <lukas@lukasandersson.com>
2023-01-23 09:05:53 +0100

    Updated auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,11 +4,11 @@ import { SECRET_KEY } from '../config/config.js'
 export const verifyToken = (req, res, next) => {
     const token = req.headers['x-access-token']
     if (!token) {
-        return res.status(403).send({ auth: false, message: 'No token provided.' })
+        return res.status(403).send({ success: false, message: 'No token provided.' })
     }
     jwt.verify(token, SECRET_KEY, (error, decoded) => {
         if (error) {
-            return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' })
+            return res.status(500).send({ success: false, message: 'Failed to authenticate token.' })
         }
         req.userId = decoded.id
         next()
