632486ba9abb4860712e18edfa6590c0f939f00f
TheoLindqvist <theo.lindqvist@telia.com>
2023-01-26 18:17:42 +0100

    home page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,13 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Flash from './Flash'
+import Team from './Team'
+import Features from './Features'
 
 export {
     Navbar,
     Footer,
     Flash,
+    Team,
+    Features,
 }
