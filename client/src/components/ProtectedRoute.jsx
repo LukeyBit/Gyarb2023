558c308a2ca04a5984141eba99dd37858fb0f9f0
LukeyBit <lukas@lukasandersson.com>
2023-02-06 10:22:02 +0100

    Moved API request to ProtectedRoute component

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, Outlet } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { checkAuth } from '../hooks'
+import * as userAPI from '../apis/userAPI'
+import secureLocalStorage from 'react-secure-storage'
 
 const ProtectedRoute = ({ requirement = true }) => {
     const [auth, setAuth] = useState(false)
@@ -9,12 +10,16 @@ const ProtectedRoute = ({ requirement = true }) => {
     const navigate = useNavigate()
 
     const getAuth = async () => {
-        const state = await checkAuth()
-        setAuth(state)
+        try {
+            const { data } = await userAPI.authUser(secureLocalStorage.getItem('token'))
+            if (data.success) return true
+        } catch (error) {
+            return false
+        }
     }
 
     useEffect(() => {
-        getAuth()
+        getAuth().then(res => setAuth(res))
         console.log('auth', auth)
         console.log('requirement', requirement)
         if (auth !== requirement) {
@@ -22,6 +27,7 @@ const ProtectedRoute = ({ requirement = true }) => {
                 dispatch({ type: 'ERROR', payload: {message: 'You must log in to visit this page'} })
                 navigate('/', { replace: true })
             } else {
+                dispatch({ type: 'ERROR', payload: {message: 'You must log out to visit this page'} })
                 navigate('/', { replace: true })
             }
         }
