4192ed7f97a658eb9bc90d9c5284ae90c2a4fb3d
LukeyBit <lukas@lukasandersson.com>
2023-02-07 23:54:55 +0100

    Implemented protected routes Fixed response for log in and out

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,41 +34,39 @@ const Navbar = () => {
     <nav className='navbar'>
       <div className='navbar__container'>
         <div className='navbar__logo'>
-          <img src={logo} className='navbar__logo-img' alt='MealMaster Logo' />
-          <span className='navbar__logo-text'>MealMaster</span>
+          <Link to='/' aria-label='MealMaster Logo Link' className='flex items-center' >
+            <img src={logo} className='navbar__logo-img' alt='MealMaster Logo' />
+            <span className='navbar__logo-text hover:text-text-color-secondary'>MealMaster</span>
+          </Link>
         </div>
         <div className='navbar__buttons'>
           <button type='button' className='navbar__cta-btn'>Get started</button>
           <button
-          data-collapse-toggle='navbar-cta'
-          type='button' className='navbar__menu-btn'
-          aria-controls='navbar-cta'
-          aria-expanded='false'
-          onClick={toggleMenu}
+            data-collapse-toggle='navbar-cta'
+            type='button' className='navbar__menu-btn'
+            aria-controls='navbar-cta'
+            aria-expanded='false'
+            onClick={toggleMenu}
           >
             <span className='sr-only'>Open main menu</span>
             <BiMenu size={30} />
           </button>
         </div>
-        <div 
-        {...(isMenuOpen ? {'data-collapse-open': true, 'aria-hidden': false} : {'data-collapse-open': false,'aria-hidden': true})
-        }
-        className={` navbar__menu ${isMenuOpen ? 'navbar__menu--open' : 'navbar__menu--closed'}`}
-        id='navbar-cta'>
+        <div
+          { ...(isMenuOpen ? { 'data-collapse-open': true, 'aria-hidden': false } : { 'data-collapse-open': false, 'aria-hidden': true }) }
+          className={` navbar__menu ${isMenuOpen ? 'navbar__menu--open' : 'navbar__menu--closed'}`}
+          id='navbar-cta'>
           <ul>
-            <li>
-              <NavLink to='/' className='navbar__menu-link' >Home</NavLink>
-            </li>
-            <li>
-              <NavLink to='/discover' className='navbar__menu-link' >Discover</NavLink>
-            </li>
             <li>
               <NavLink to='/search' className='navbar__menu-link' >Search</NavLink>
             </li>
-            { auth ? (
+            {auth ? (
               <>
                 <li>
-                <NavLink to='/profile' className='navbar__menu-link' >Profile</NavLink>
+                  <NavLink to='/discover' className='navbar__menu-link' >Discover</NavLink>
+                </li>
+                <li>
+                  <NavLink to='/profile' className='navbar__menu-link' >Profile</NavLink>
                 </li>
                 <li>
                   <Link to='/' onClick={handleLogout} className='navbar__menu-link navbar__menu-link__logout' >Log out</Link>
--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,30 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useNavigate, Outlet } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import secureLocalStorage from 'react-secure-storage'
 
 const ProtectedRoute = ({ requirement = true }) => {
     const [auth, setAuth] = useState(secureLocalStorage.getItem('isAuthorized') || false)
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    
+    const response = useSelector(state => state.response)
 
-
-    useEffect(() => {
+    const checkAuth = useCallback(() => {
         setAuth(secureLocalStorage.getItem('isAuthorized') || false)
         if (auth !== requirement) {
-            if (requirement) {
-                dispatch({ type: 'ERROR', payload: {message: 'You must log in to visit this page'} })
-                navigate('/', { replace: true })
-            } else {
-                dispatch({ type: 'ERROR', payload: {message: 'You must log out to visit this page'} })
-                navigate('/', { replace: true })
+            if (!response.message) { 
+                if (requirement) {
+                    dispatch({ type: 'ERROR', payload: { message: 'You must log in to visit this page' } })
+                } else {
+                    dispatch({ type: 'ERROR', payload: { message: 'You must be logged out to visit this page' } })
+                }
             }
+            navigate('/', { replace: true })
         }
-    }, [auth, navigate, requirement, dispatch])
+    }, [auth, requirement, response.message, dispatch, navigate])
 
-  return (
-    <Outlet />
-  )
+    useEffect(() => {
+        checkAuth()
+    }, [auth, checkAuth])
+
+    return (
+        <Outlet />
+    )
 }
 
 export default ProtectedRoute
