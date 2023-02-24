6b1e2abbc9b064aff40a01fec03b3205b77a03ba
LukeyBit <lukas@lukasandersson.com>
2023-02-24 15:02:10 +0100

    Arbete med filter

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -1,42 +1,78 @@
 import React, { useState } from 'react'
 
 const Filters = () => {
-  const [dropdownOpen, setDropdownOpen] = React.useState(false)
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const [filters, setFilters] = useState([
+    'Vegan',
+    'Vegetarian',
+    'Pescatarian',
+    'Lacto-Vegetarian',
+    'Gluten Free',
+    'Dairy Free',
+    'Nut Free',
+    'Soy Free',
+    'Egg Free',
+    'Peanut Free',
+    'Tree Nut Free',
+    'Fish Free',
+    'Shellfish Free',
+    'Sesame Free',
+    'Wheat Free',
+    'Sulfite Free',
+    'Mustard Free',
+    'Celery Free',
+    'Lupine Free',
+    'Mollusk Free',
+    'Kosher',
+    'Halal',
+  ].sort())
+
+  const [selectedFilters, setSelectedFilters] = useState([])
 
   const openMenu = () => {
     setDropdownOpen(!dropdownOpen)
   }
 
+  const filterChange = (e) => {
+    e.preventDefault()
+    console.log(e.target.name)
+    if (selectedFilters.includes(e.target.name)) {
+      setSelectedFilters(selectedFilters.filter((filter) => filter !== e.target.name))
+      setFilters([...filters, e.target.name].sort())
+    } else {
+      setSelectedFilters([...selectedFilters, e.target.name].sort())
+      setFilters(filters.filter((filter) => filter !== e.target.name))
+    }
+  }
+
   return (
-    <div className='mr-10 w-1/6 h-[calc(100vh-5vh)] scroll-m-1 top-0 bg-white drop-shadow-2xl' >
+    <div className='mr-10 w-1/6 h-[calc(100vh-5vh)] scroll-m-1 top-0 bg-white drop-shadow-xl' >
       <div className='flex flex-col justify-center'>
 
-        <button id='dropdownBgHoverButton' data-dropdown-toggle='dropdownBgHover' className='subtitle-font w-full flex flex-row justify-center align-middle hover:bg-secondary' type='button' onClick={openMenu}>Dropdown checkbox
+        <button id='dropdownBgHoverButton' data-dropdown-toggle='dropdownBgHover' className='text-font w-full flex flex-row justify-center align-middle hover:bg-secondary' type='button' onClick={openMenu}>Dropdown checkbox
           <svg className='w-4 h-4 ml-2 my-auto' aria-hidden='true' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-            <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M19 9l-7 7-7-7'></path>
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M19 9l-7 7-7-7'></path>
           </svg>
         </button>
 
-        <div id='dropdownBgHover' className={`z-10 w-48 bg-white rounded-lg shadow dark:bg-gray-700 ${dropdownOpen ? null : 'hidden'}`} >
-          <ul className='p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200' aria-labelledby='dropdownBgHoverButton'>
-            <li>
-              <div className='flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600'>
-                <input id='checkbox-item-4' type='checkbox' value='' className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500' />
-                <label htmlFor='checkbox-item-4' className='w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300'>Default checkbox</label>
-              </div>
-            </li>
-            <li>
-              <div className='flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600'>
-                <input id='checkbox-item-5' type='checkbox' value='' className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500' />
-                <label htmlFor='checkbox-item-5' className='w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300'>Checked state</label>
-              </div>
-            </li>
-            <li>
-              <div className='flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600'>
-                <input id='checkbox-item-6' type='checkbox' value='' className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500' />
-                <label htmlFor='checkbox-item-6' className='w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300'>Default checkbox</label>
-              </div>
-            </li>
+        <div id='dropdownBgHover' className={`z-10 w-full rounded-lg ${dropdownOpen ? null : 'hidden'}`} >
+          <ul className='text-sm h-[70vh] w-full overflow-y-scroll' aria-labelledby='dropdownBgHoverButton'>
+            {selectedFilters.map((filter) => (
+              <li key={filter} className='h-10 flex flex-row justify-start align-middle border-2 border-primary'>
+                <input id={filter} type='checkbox' name={filter} className='peer' onChange={filterChange} checked />
+                <label className='flex flex-row justify-center align-center text-center peer-checked' htmlFor={filter}>
+                  {filter}
+                </label>
+              </li>
+            ))}
+            {filters.map((filter) => (
+              <li key={filter} id={filter} className='h-10 flex flex-row justify-start align-middle'>
+                <input type='checkbox' id={filter} name={filter} className='peer' onChange={filterChange} />
+                <label htmlFor={filter} className='peer-checked flex flex-row justify-center align-center text-center'>
+                  {filter}
+                </label>
+              </li>
+            ))}
           </ul>
         </div>
 
