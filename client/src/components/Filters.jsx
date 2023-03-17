16af46b97e8339a1f9eb4d8a3aae9f1caf3f3b95
LukeyBit <lukas@lukasandersson.com>
2023-03-17 20:07:28 +0100

    FIxed filter for different viewports

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Filter from './Filter'
 import { params } from '../apis/recipeAPI'
-import { BiFilter } from 'react-icons/bi'
+import { BiFilter, BiX } from 'react-icons/bi'
+import { useWindowDimensions } from '../hooks'
 
 const Filters = () => {
   const recipeParams = params.recipeParams
   const [open, setOpen] = useState(false)
+  const { width } = useWindowDimensions()
 
 
   const handleReset = () => {
@@ -17,20 +19,29 @@ const Filters = () => {
     setOpen(!open)
   }
 
+  useEffect(() => {
+    if (width > 1024) {
+      setOpen(false)
+    }
+  }, [width])
+
   return (
     <>
-      <button className='lg:hidden fixed bottom-8 left-8 bg-primary rounded-full p-1.5 text-white'
+      <button className={`fixed bottom-8  bg-primary rounded-full p-1.5 text-white z-[100] transition-all lg:hidden ${open ? 'left-[50%] right-[50%]' : 'left-8'} `}
         onClick={toggleMenu}>
-        <BiFilter className='w-8 h-8' />
+        {!open
+          ?<BiFilter className='w-8 h-8' />
+          :<BiX className='w-8 h-8' />
+        }
       </button>
-      <div className={`lg:mr-10 lg:w-80 lg:min-h-full lg:scroll-m-1 lg:flex top-0 bg-white drop-shadow-xl w-full ${!open ? 'hidden' : 'fixed top-0 left-0 w-full min-h-screen z-1000'}`}>
-        <div className='lg:flex lg:flex-col '>
+        <div className={`lg:mr-10 lg:w-80 lg:min-h-full lg:scroll-m-1 lg:flex top-0 bg-white lg:z-auto drop-shadow-xl w-full ${open ? 'fixed top-0 left-0 w-full min-h-screen z-50' : 'hidden'}`}>
+        <div className='lg:flex lg:flex-col lg:pt-0 pt-6'>
           {
             Object.keys(recipeParams).map((key) => (
               <Filter key={key} filterName={key} filterItems={recipeParams[key]} />
             ))
           }
-          <div className='flex justify-center'>
+          <div className='flex lg:justify-center lg:ml-0 justify-start ml-6'>
             <button className='text-font text-white bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-secondary font-medium rounded-lg px-3 py-2 mb-6 mt-4' onClick={handleReset} >Clear filters</button>
           </div>
         </div>
--- client/src/containers/Search.jsx
+++ client/src/containers/Search.jsx
@@ -44,7 +44,7 @@ const Search = () => {
   return (
     <div className='flex flex-row justify-between min-h-[calc(100vh-7rem)]'>
       <Filters />
-      <div className='flex flex-col w-full pr-10'>
+      <div className='flex flex-col w-full lg:pr-10'>
         <form className='flex flex-row mt-8 w-full justify-center align-middle mb-8' onSubmit={handleSearchSubmit} >
           <div className='flex flex-row w-full justify-center align-top'>
             <input type='text' id='search' className='subtitle-font justify-center align-middle bg-gray-50 border-gray-300 border text-text-color-primary sm:text-sm rounded-lg focus:ring-primary focus:outline-none focus:border-primary focus:border block w-3/4 p-2.5 mr-4' placeholder='Keywords separated by comma' onChange={handleSearchChange} />
