bea04051cf5d0a1ac79d41bcae4fe5b510d5dab8
LukeyBit <lukas@lukasandersson.com>
2023-03-21 14:00:20 +0100

    Updated footer styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import badge from '../assets/edamam_badge.svg'
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col md:flex-row justify-center items-center p-2 bg-fill-color w-full border-t-4 border-t-primary' >
+    <footer className='flex flex-col md:flex-row justify-center md:gap-0 gap-2 items-center p-2 bg-fill-color w-full border-t-4 border-t-primary' >
         <p className='text-center text-sm text-font text-text-color-primary mr-4' >© 2023 | Created by Lukas Andersson and Theo Lindqvist</p>
         <a href="https://www.edamam.com" target="_blank" className='bg-white rounded-xl' rel="noopener noreferrer">
           <img src={badge} className='h-10' alt='Search powered by Edamam' />
