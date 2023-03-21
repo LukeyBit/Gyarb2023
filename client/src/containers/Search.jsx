4d5449afc53cc5e07bdd40e42a69cd6f8d842083
LukeyBit <lukas@lukasandersson.com>
2023-03-21 13:57:47 +0100

    Fixed SVG to Bi-Icon

--- client/src/containers/Search.jsx
+++ client/src/containers/Search.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { Filters, Result } from '../components'
 import { getRecipes, getNextRecipes } from '../apis/recipeAPI'
-import { BiUpArrow } from 'react-icons/bi'
+import { BiUpArrow, BiSearch } from 'react-icons/bi'
 
 const Search = () => {
   const noResultsMessage = useRef('Search for recipes by keywords and/or filters')
@@ -50,7 +50,7 @@ const Search = () => {
             <input type='text' id='search' className='subtitle-font justify-center align-middle bg-gray-50 border-gray-300 border text-text-color-primary sm:text-sm rounded-lg focus:ring-primary focus:outline-none focus:border-primary focus:border block w-3/4 p-2.5 mr-4' placeholder='Keywords separated by comma' onChange={handleSearchChange} />
             <label htmlFor='search' className='sr-only'>Search</label>
             <button type='submit' className='text-font text-white bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-secondary font-medium rounded-lg px-3 py-2.5' onClick={handleSearchSubmit} >
-              <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path></svg>
+              <BiSearch className='w-5 h-5' />
               <span className='sr-only'>Search</span>
             </button>
           </div>
