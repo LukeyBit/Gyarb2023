6ec564963e6b8a8bb174e1409b47b3edf99da709
LukeyBit <lukas@lukasandersson.com>
2023-03-21 14:37:35 +0100

    Updated responsive footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,8 +4,8 @@ import badge from '../assets/edamam_badge.svg'
 const Footer = () => {
   return (
     <footer className='flex flex-col md:flex-row justify-center md:gap-0 gap-2 items-center p-2 bg-fill-color w-full border-t-4 border-t-primary' >
-        <p className='text-center text-sm text-font text-text-color-primary mr-4' >© 2023 | Created by Lukas Andersson and Theo Lindqvist</p>
-        <a href="https://www.edamam.com" target="_blank" className='bg-white rounded-xl' rel="noopener noreferrer">
+        <p className='text-center text-sm text-font text-text-color-primary mr-4 md:order-1 order-2' >© 2023 | Created by Lukas Andersson and Theo Lindqvist</p>
+        <a href="https://www.edamam.com" target="_blank" className='md:order-2 order-1 bg-white rounded-xl' rel="noopener noreferrer">
           <img src={badge} className='h-10' alt='Search powered by Edamam' />
         </a>
     </footer>
